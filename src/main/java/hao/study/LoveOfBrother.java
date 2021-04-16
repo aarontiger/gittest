8ec8176bfe7eb35604a6ead9ec25e14ac8779d7c
‘haoshuhu <shuhuhao@163.com>
2021-04-16 15:37:34 +0800

    add love class

---
+++ src/main/java/hao/study/LoveOfBrother.java
@@ -0,0 +1,9 @@
+package hao.study;
+
+public class LoveOfBrother {
+
+    public void toBeFriendWithYou(){
+        System.out.println("I love you more than I can say");
+        System.out.println("Great thanks for you receiving my gift,I am very happy");
+    }
+}
